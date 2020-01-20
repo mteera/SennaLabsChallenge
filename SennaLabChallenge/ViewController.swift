16cd2a28ab6d6d291425a0755c9937253dc94a4d
Chace Teera <hello@chaceteera.com>
2020-01-20 08:55:21 +0000

    Implemented Hello World in the console and alert dialog.

--- SennaLabChallenge/ViewController.swift
+++ SennaLabChallenge/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Hello World")
+        
+
     }
 
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let alertController = UIAlertController(title: "Hello World", message: nil, preferredStyle: .alert)
+        
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: { (action) in
+            let seconds: Double = 0
+            DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: {
+                self.dismiss(animated: true, completion: nil)
+
+            })
+            
+        })
+        alertController.addAction(dismissAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
