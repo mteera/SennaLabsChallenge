b8df917fcd5adcecbf5ef9a0cb86e0d5ea38503d
Chace Teera <hello@chaceteera.com>
2020-01-20 08:44:31 +0000

    Initial Commit

---
+++ SennaLabChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SennaLabChallenge
+//
+//  Created by Chace Teera on 20/01/2020.
+//  Copyright © 2020 chaceteera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
